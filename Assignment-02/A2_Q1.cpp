ddbbe2123d2d8998e59f805022e87231fbbfcbdb
Tushar <bl3z1965@gmail.com>
2026-08-20 10:56:11 +0530

    Commit

--- Assignment-02/A2_Q1.cpp
+++ Assignment-02/A2_Q1.cpp
@@ -5,14 +5,17 @@ using namespace std;
 class Student{
     
     private:
+
         string name;
         int rollNo;
 
     public:
+
         Student(){
             name = "Default";
             rollNo = 0;
         }
+
     void display(){
         cout << "Name: " << name << endl;
         cout << "Roll Number: " << rollNo << endl;
@@ -20,6 +23,9 @@ class Student{
 };
 
 int main(){
+
     Student s;
     s.display();
+
+    return 0;
 }
